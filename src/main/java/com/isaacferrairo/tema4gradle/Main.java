88e9d9cad36b20fc0a3aff4ae6902db12d312756
isaac <isaacferrairo@gmail.com>
2026-02-13 18:55:40 +0100

    Pérdida de contexto

--- src/main/java/com/isaacferrairo/tema4gradle/Main.java
+++ src/main/java/com/isaacferrairo/tema4gradle/Main.java
@@ -1,5 +1,11 @@
 package com.isaacferrairo.tema4gradle;
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ChatMessage;
+import dev.langchain4j.data.message.UserMessage;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void  main(String[] args){
@@ -8,14 +14,21 @@ public class Main {
 
             // El TOKEN no es necesario para interactuar con modelos locales
             final String TOKEN = "PEGA_AQUI_TU_TOKEN";
-
             var model = OpenAiChatModel.builder()
-                    .baseUrl("http://localhost:11434/v1")
+                    .baseUrl("https://localhost:11434/v1")
                     .apiKey(TOKEN)
                     .modelName("gemma:2b")
                     .build();
-            String respuesta = model.chat("Cuéntame un chiste");
-            System.out.println(respuesta);
+            List<ChatMessage> history = new ArrayList<>();
+            // Interacción 1
+            history.add(new UserMessage("Hola, soy Isaac"));
+            AiMessage respuesta = model.chat(history).aiMessage();
+            history.add(respuesta);
+            // Interacción 2
+            history.add(new UserMessage("¿Recuerdas cómo me llamo?"));
+            respuesta = model.chat(history).aiMessage();
+            history.add(respuesta);
+            System.out.println(respuesta.text());
 
 
     }
