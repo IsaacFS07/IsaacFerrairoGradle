72c1ff3c6b3af1e6285ad6e4b3643ffecde7d2f3
isaac <isaacferrairo@gmail.com>
2026-02-13 18:52:47 +0100

    Prueba API

--- src/main/java/com/isaacferrairo/tema4gradle/Main.java
+++ src/main/java/com/isaacferrairo/tema4gradle/Main.java
@@ -1,16 +1,21 @@
 package com.isaacferrairo.tema4gradle;
+import dev.langchain4j.model.openai.OpenAiChatModel;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void  main(String[] args){
 
 
 
+            // El TOKEN no es necesario para interactuar con modelos locales
+            final String TOKEN = "PEGA_AQUI_TU_TOKEN";
 
-
-        
-
+            var model = OpenAiChatModel.builder()
+                    .baseUrl("http://localhost:11434/v1")
+                    .apiKey(TOKEN)
+                    .modelName("gemma:2b")
+                    .build();
+            String respuesta = model.chat("Cuéntame un chiste");
+            System.out.println(respuesta);
 
 
     }
